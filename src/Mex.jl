6ab19289d0ec317d0bd608a55a3256113309ebf9
Jeremy Bejanin <jeremy.bejanin@gmail.com>
2021-01-26 10:28:22 -0500

    patch MATLAB.jl's handling of reshaped arrays

--- src/Mex.jl
+++ src/Mex.jl
@@ -54,4 +54,23 @@ function MATLAB.mxarray(a::Base.ReinterpretArray{T}) where T<:MATLAB.MxComplexNu
     return mx
 end
 
+# Patch MATLAB.jl's handling of reshaped arrays
+function MATLAB.mxarray(a::Base.ReshapedArray{T}) where T<:MATLAB.MxRealNum
+    mx = mxarray(T, size(a))
+    ccall(:memcpy, Ptr{Cvoid}, (Ptr{Cvoid}, Ptr{Cvoid}, UInt), data_ptr(mx), parent(a), length(a)*sizeof(T))
+    return mx
+end
+
+function MATLAB.mxarray(a::Base.ReshapedArray{T}) where T<:MATLAB.MxComplexNum
+    mx = mxarray(T, size(a))
+    na = length(a)
+    rdat = unsafe_wrap(Array, MATLAB.real_ptr(mx), na)
+    idat = unsafe_wrap(Array, MATLAB.imag_ptr(mx), na)
+    @inbounds for i = 1:na
+        rdat[i] = real(a[i])
+        idat[i] = imag(a[i])
+    end
+    return mx
+end
+
 end # module
