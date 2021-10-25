9534827d4236690ddd64e6894f5676a3814e46a7
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-10-24 23:21:22 -0600

    update MATLAB tests

--- test/jlevalTest.m
+++ test/jlevalTest.m
@@ -1,11 +1,14 @@
 %% tests for jl.eval function
 
+%% Test 1: Single Return Argument
 result = jl.eval('2+2');
 assert(result == 4)
 
+%% Test 2: Multiple Return Arguments
 [s, c] = jl.eval('sin(pi/3), cos(pi/3)');
-assert(s = sin(pi/3))
-assert(c = cos(pi/3))
+assert(s == sin(pi/3))
+assert(c == cos(pi/3))
 
-jleval 1 + 1
-assert(ans == 2)
+%% Test 3: Command Syntax
+jleval 1 + 1;
+assert(isequal(ans, 2))
--- test/jlmexTest.m
+++ test/jlmexTest.m
@@ -1,8 +1,9 @@
 %% tests for jl.mex function
+
 a = rand(5,5);
 jleval import MATLAB;
 jleval double_it(args::Vector{MATLAB.MxArray}) = [2*MATLAB.jvalue(arg) for arg in args];
 result = jl.mex('double_it', a);
 result = result{1};
 expected_result = 2*a;
-assert (result == expected_result)
+assert (isequal(result, expected_result))
