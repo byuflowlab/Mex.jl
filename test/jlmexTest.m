0e60189b967e91037b2dabed95f635e6f5f4b70f
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-10-25 17:10:29 -0600

    modify tests to work with github actions

--- test/jlmexTest.m
+++ test/jlmexTest.m
@@ -5,8 +5,7 @@ assert(exist("mexjulia", "file") == 3)
 
 %% Test 1: jl.mex
 a = rand(5,5);
-jleval import MATLAB;
-jleval double_it(args::Vector{MATLAB.MxArray}) = [2*MATLAB.jvalue(arg) for arg in args];
+jleval double_it(args::Vector{Mex.MxArray}) = [2*Mex.jvalue(arg) for arg in args];
 result = jl.mex('double_it', a);
 result = result{1};
 expected_result = 2*a;
