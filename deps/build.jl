47c27edd8dbca3129b7d36871d6ad2dfc78482f2
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-11-01 09:49:31 -0600

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -54,7 +54,7 @@ function ldlibs()
     if Sys.isunix()
         return "-l$libname -ldl"
     else
-        return normpath(joinpath(libDir(), "..", "lib", "lib$libname.dll.a"))
+        return "$(normpath(joinpath(libDir(), "..", "lib", "lib$libname.dll.a"))) -lopenlibm"
     end
 end
 
